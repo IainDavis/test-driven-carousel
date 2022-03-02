c7ac75579fcbd7b39b3f3eb8b8cd1ccacabd3c9b
Iain Davis <iain.s.davis@gmail.com>
2022-03-02 13:58:44 +1100

    :wrench: Initial ESLint and Prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+// .eslintrc.js
+
+module.exports = {
+  plugins: ['react'],
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    node: true,
+  },
+  settings: {
+    react: {
+      version: '16.4.2',
+    },
+  },
+  rules: {
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'comma-dangle': ['error', 'always-multiline'],
+  },
+};
